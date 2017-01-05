be85450bd31dbf7804ac9ce2dac87d3ad540d256
minan05 <minda.aguhob@gmail.com>
2017-01-04 23:42:06 -0500

    Update app.js
    In Github, just pasted Hello World code from https://github.com/SarahSays/BotWorks/blob/master/app.js 
    over old code from some other Hello World repo that didn't work.

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
-app.get('/', function(req, res){
- res.sendFile(__dirname + '/index.html');
+//Add your requirements
+var restify = require('restify');
+var builder = require('botbuilder');
+
+var appId = process.env.MY_APP_ID || "Missing your app ID";
+var appSecret = process.env.MY_APP_SECRET || "Missing your app secret";
+
+// Create bot and add dialogs
+var bot = new builder.BotConnectorBot
+({appId: process.env.MY_APP_ID, appSecret: process.env.MY_APP_SECRET});
+bot.add('/', new builder.SimpleDialog( function (session) {
+session.send('Hello World');
+}));
+
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3000, function () {
+console.log('%s listening to %s', server.name, server.url);
 });
-io.on('connection', function(socket){
- socket.on('chat message', function(msg){
- io.emit('chat message', msg);
- });
-});
-http.listen(port, function(){
- console.log('listening on ' + port);
-});
+
+server.get('/', restify.serveStatic({
+    directory: __dirname,
+    default: '/index.html'
+}));
